c564509a92b3e9edf6c85ec0a5c0d6a2703795c1
Philomina Peter <CS5919@cloudium.local>
2021-09-20 09:40:18 +0530

    document

--- myApp.js
+++ myApp.js
@@ -76,8 +76,14 @@ const findAndUpdate = (personName, done) => {
   })
 };
 
-const removeById = (personId, done) => {
-  done(null /*, data*/);
+var removeById = function(personId, done) {
+  Person.findByIdAndRemove(
+    personId,
+    (err, removedDoc) => {
+      if(err) return console.log(err);
+      done(null, removedDoc);
+    }
+  ); 
 };
 
 const removeManyPeople = (done) => {
