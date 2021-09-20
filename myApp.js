5fa598f3f24fb9a89087300ccfa1aac395c1d72f
Philomina Peter <CS5919@cloudium.local>
2021-09-20 09:34:35 +0530

    edit

--- myApp.js
+++ myApp.js
@@ -57,10 +57,16 @@ var findPersonById = function(personId, done) {
 };
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
-
-  done(null /*, data*/);
+  Person.findById(personId, (err, person) => {
+    if(err) return console.log(err); 
+    person.favoriteFoods.push(foodToAdd);
+    person.save((err, updatedPerson) => {
+      if(err) return console.log(err);
+      done(null, updatedPerson)
+    })
+  })
 };
-
+  
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
